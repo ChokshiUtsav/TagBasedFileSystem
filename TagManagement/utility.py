7208ff6dfc6941127931013db9942fef6be9289d
Deeksha Thakur <deekshathakur11@gmail.com>
2017-04-24 16:35:51 +0530

    improved tag suggestions

--- TagManagement/utility.py
+++ TagManagement/utility.py
@@ -3,12 +3,6 @@ from nltk.tag import pos_tag
 from collections import defaultdict
 import mimetypes
 
-def isTextfile(filename):
-    # if filetype text or None, return True
-    if mimetypes.guess_type(filename,strict=True)[0] == 'text/plain' or not mimetypes.guess_type(filename,strict=True)[0]:
-        return True
-    else:
-        return False
 def findFileType(filename):
     ftype = mimetypes.guess_type(filename,strict=True)[0]
     if ftype:
@@ -19,3 +13,8 @@ def findFileType(filename):
             return p[0]
     else: # if file type is null, return text tag for file
         return "text"
+
+def getTopNDictItems(dct,n):    # extract n most frequent named entities from file
+    print "topn",[pair[0] for pair in sorted(dct.items(), key=lambda dct: dct[1], reverse = True)][:n]
+
+    return [pair[0] for pair in sorted(dct.items(), key=lambda dct: dct[1], reverse = True)][:n]
