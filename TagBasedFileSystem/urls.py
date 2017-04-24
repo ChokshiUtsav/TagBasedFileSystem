c725407ec9a8906383158756965b10b108197bc4
ChokshiUtsav <uvc.smart.29@gmail.com>
2017-04-24 22:11:45 +0530

    Browse And Search UI along with graph creation

--- TagBasedFileSystem/urls.py
+++ TagBasedFileSystem/urls.py
@@ -8,7 +8,9 @@ urlpatterns =[
     # url(r'^$', 'TagBasedFileSystem.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^$', tagviews.index ),
-	url(r'^tag/', include('TagManagement.urls') ),
+	url(r'^tag/', include('TagManagement.urls')),
+    url(r'^search/', include('FileSearch.urls')),
     url(r'^admin/', include(admin.site.urls)),
+
     
 ]
