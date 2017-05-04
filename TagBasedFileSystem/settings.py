b8868ac22f2d6713011de881b6b6dcf63de1b5f8
ChokshiUtsav <uvc.smart.29@gmail.com>
2017-05-04 19:45:51 +0530

    Removed templates/assigntag folder

--- TagBasedFileSystem/settings.py
+++ TagBasedFileSystem/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'resources',
     'TagManagement',
+    'UserSettings',
 ]
 
 MIDDLEWARE = [
---
+++ UserSettings/views.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+# Create your views here.
+
+# Set of Settings
+'''
+1) : Set Mount Directory (default : home dir)
+2) : Home Screen : Notification Time-out (default : 24hrs)
+3) : Option for Adding folder hierarchy as tags
+4) : Running cron jobs 
+5) : status of various cron jobs (options to start, restart and stop them)
+'''
+
+'''
+setup.sh
+set HOME variable in config.py
